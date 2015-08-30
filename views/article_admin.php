4ca6ee5da8e2103cc92a5dea1f25318a8a5ca953
Stanislav Medvedev <medvedev_sv@taximaxim.ru>
2015-08-30 12:58:10 +0300

    first commit:

---
+++ views/article_admin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Мой первый блог</title>
+        <link rel="stylesheet" href="../style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Мой первый блог</h1>
+            <div>
+                <form method="post" action="index.php?action=<?=$_GET['action']?>&id=<?=$_GET['id']?>">
+                    <label>Название
+                        <input type="text" name="title" value="<?=$article['title']?>" class="form-item" autofocus required>
+                    </label></p>
+                    <label>Дата
+                        <input type="date" name="date" value="<?=$article['date']?>" class="form-item" required>
+                    </label></p>
+                    <label>Содержимое
+                        <textarea  name="content" class="form-item" required><?=$article['content']?></textarea>
+                    </label></p>
+                    <input type="submit" value="Сохранить" class="btn">
+                </form>
+            </div>
+            <footer>
+                <p>Мой первый блог <br> Copyright &copy; 2015</p>
+            </footer>
+        </div>
+    </body>
+</html>
